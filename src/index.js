93952bb0d3ed1420d43018234bb2d39ecd9e0063
saud ikram <saudikram62@gmail.com>
2022-11-29 13:29:00 +0000

    challenge 2 completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
 import React from "react";
 import ReactDOM from "react-dom";
-const fname = "saud";
-const lname = "Ikram";
-const num = 11;
+var name = "saud Ikram";
+var date = new Date().getFullYear();
 ReactDOM.render(
   <div>
-    <h1>Hello {`${fname} ${lname}`}!</h1>
-    <p>your lucky number is: {num}</p>
+    <p>created by {name}</p>
+    <p>Copyright {date}</p>
   </div>,
   document.getElementById("root")
 );
