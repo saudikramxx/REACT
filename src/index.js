deebdde8ef0fdbfc18606f920ce4275f115921a7
saud ikram <116587678+saudikramxx@users.noreply.github.com>
2022-11-30 19:24:39 +0530

    challenge 5 completed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-//Create a react app from scratch.
-//It should display 2 paragraph HTML elements.
-//The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
 import React from "react";
 import ReactDOM from "react-dom";
-var name = "saud Ikram";
-var date = new Date().getFullYear();
-ReactDOM.render(
-  <div>
-    <p>created by {name}</p>
-    <p>Copyright {date}</p>
-  </div>,
-  document.getElementById("root")
-);
+import App from "./components/app";
+
+ReactDOM.render(<App />, document.getElementById("root"));
